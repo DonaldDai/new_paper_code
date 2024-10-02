8b7a34bb2e2271c42144eae248814d7cbde55a22
Yichao <yichao1liu@gmail.com>
2024-10-01 22:00:48 -0500

    refactor: 优化slurm脚本输出

--- sbatch.sh
+++ sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=new_clm_pretrain    # 作业名称
-#SBATCH --nodes=2                  # 请求的节点数
+#SBATCH --nodes=4                  # 请求的节点数
 #SBATCH --ntasks-per-node=1         # 每个节点上的任务数
 #SBATCH --output=./logs/clm_pretrain_%j.out # 标准输出文件
 #SBATCH --error=./logs/clm_pretrain_%j.err  # 标准错误文件
@@ -26,5 +26,6 @@ MASTER_ADDR=$(hostname)
 
 # 执行PyTorch分布式训练命令
 # 端口最好使用随机的
+# seq,base,target,seq_esm
 t="seq"
-srun bash -c "torchrun --nproc_per_node=3 --nnodes=2 --node_rank=\$SLURM_NODEID --master_addr=$MASTER_ADDR --master_port=29500 train.py --model-choice transformer --num-epoch 200 --data-type ${t} --data-path /work/09735/yichao/ls6/zhilian/new_paper_code  --save-directory /work/09735/yichao/ls6/zhilian/new_paper_code/pretrain_${t}"
+srun bash -c "torchrun --nproc_per_node=3 --nnodes=4 --node_rank=\$SLURM_NODEID --master_addr=$MASTER_ADDR --master_port=29500 train.py --model-choice transformer --num-epoch 200 --data-type ${t} --data-path /work/09735/yichao/ls6/zhilian/new_paper_code  --save-directory /work/09735/yichao/ls6/zhilian/new_paper_code/pretrain_${t}_cut"
